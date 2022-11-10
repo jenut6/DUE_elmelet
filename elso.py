71967c03e5d6a5fd9ae45708c6b01c9b35d84353
jenut6 <92020997+jenut6@users.noreply.github.com>
2022-11-10 09:23:45 +0100

    Add files via upload

---
+++ elso.py
@@ -0,0 +1,1 @@
+print("Üdv a pythonban.")
---
+++ main.py
@@ -0,0 +1,12 @@
+data = int(input("Adjon meg egy időt 0 - 24 között "))
+
+
+if(data < 24 and data > 0):
+    print(data)
+else:
+    data = int(input("Adjon meg egy időt 0 - 24 között "))
+
+
+
+
+
